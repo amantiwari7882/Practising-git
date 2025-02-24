0dcc685bbd6ac6663c88c53423f854a66a885e07
amantiwari7882 <amantiwari7882@gmail.com>
2025-02-24 11:57:22 +0530

    Modification in files

--- Demo.cpp
+++ Demo.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     int a,b,c;
+    cout<<"Enter three numbers: "<<endl;
     cin>>a>>b>>c;
         if(a>b && a>c){
             cout<<a<<" is greatest of all.";
---
+++ tutorial.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // for (int i = 0; i <=50; i++)
+    // {
+    //    cout << i << endl;
+    // }
+    
+    
+    // int i = 1;
+    // while (i <= 50){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    
+    // int i = 1;
+    // do
+    // {
+    //     cout<<6*i<<endl;
+    //     i++;
+    // } while (i<=20);
+    
+    return 0;
+}
